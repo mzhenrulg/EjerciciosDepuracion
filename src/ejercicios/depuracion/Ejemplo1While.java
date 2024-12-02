bb98b2d2ef87df72bc3f765503dcdde5e2c6dae8
mzhenrulg69 <mzhenrulg69@educastur.es>
2024-12-02 21:44:58 +0100

    cambio a numero

--- src/ejercicios/depuracion/Ejemplo1While.java
+++ src/ejercicios/depuracion/Ejemplo1While.java
@@ -1,16 +1,22 @@
 package ejercicios.depuracion;
 
 import java.util.Scanner;
+
 public class Ejemplo1While {
+    private int suma,numero;
+    public Ejemplo1While(){
+        this.suma=0;
+        this.numero=0;
+    }
     public static void main(String[] args) {
-        int suma = 0, num;
+        int suma = 0, numero;
         Scanner sc = new Scanner(System.in);
         System.out.print("Introduzca un número, por favor: ");
-        num = sc.nextInt();
-        while (num>= 0){
-               suma = suma + num;
+        numero= sc.nextInt();
+        while (numero>= 0){
+               suma = suma + numero;
                System.out.print("Introduzca un número por favor: ");
-               num = sc.nextInt();
+               numero = sc.nextInt();
         }
         System.out.println("La suma es: " + suma );
     }
