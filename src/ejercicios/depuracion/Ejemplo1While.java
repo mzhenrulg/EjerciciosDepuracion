acd04a86a3cfe4a751c7812952607a3c02577695
mzhenrulg69 <mzhenrulg69@educastur.es>
2024-12-02 21:45:57 +0100

    cambio a contador

--- src/ejercicios/depuracion/Ejemplo1While.java
+++ src/ejercicios/depuracion/Ejemplo1While.java
@@ -3,10 +3,10 @@ package ejercicios.depuracion;
 import java.util.Scanner;
 
 public class Ejemplo1While {
-    private int suma,numero;
+    private int suma,contador;
     public Ejemplo1While(){
         this.suma=0;
-        this.numero=0;
+        this.contador=0;
     }
     public static void main(String[] args) {
         int suma = 0, numero;
